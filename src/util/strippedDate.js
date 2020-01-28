d56a615a85e619e2f8ba59046cc9c161e030f2d3
Henry Bulmer <hbulmer@blkmtn.com>
2020-01-27 22:11:47 -0800

    Date stripper can handle iso strings

--- src/util/strippedDate.js
+++ src/util/strippedDate.js
@@ -1,9 +1,21 @@
+const _ = require( 'lodash' );
+
 function strippedDate( dateTime ){
     if ( !dateTime )
         return "";
 
+    if ( isISOString( dateTime ) )
+        return dateTime;
+
     var parsed = new Date( dateTime );
     return new Date( Date.UTC( parsed.getFullYear(), parsed.getMonth(), parsed.getDate() ) ).toISOString();
+}
+
+function isISOString( dateTime ){
+    const isoStringRegex = "^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]+Z$";
+    
+    return _.isString( dateTime ) 
+        && dateTime.match( isoStringRegex )
 }
 
 module.exports = {
